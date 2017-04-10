2380a638a0c49377c3ddd2d2056372c253f5f984
Adrian Malacoda <adrian.malacoda@monarch-pass.net>
2017-04-09 20:33:41 -0500

    Add a function to get the hostname from a resource url

--- src/client/urlbuilder.rs
+++ src/client/urlbuilder.rs
@@ -1,4 +1,8 @@
+use url::Url;
+use url::ParseError;
 use url::percent_encoding::{utf8_percent_encode, FORM_URLENCODED_ENCODE_SET};
+
+use std::str::FromStr;
 
 // Helper functions
 pub fn get_https_url(hostname: &str, resource: &str) -> String {
@@ -7,6 +11,21 @@ pub fn get_https_url(hostname: &str, resource: &str) -> String {
 
 pub fn get_http_url(hostname: &str, resource: &str) -> String {
     get_url(hostname, resource, "http")
+}
+
+pub fn get_hostname(url_str: &str) -> Result<String, ParseError> {
+    match Url::from_str(url_str) {
+        Ok(url) => {
+            url.host().map(|host| format!("{}", host)).ok_or_else(|| ParseError::EmptyHost)
+        },
+        Err(err) => {
+            if !url_str.starts_with("acct:") {
+                return get_hostname(&format!("acct:{}", url_str));
+            }
+
+            Result::Err(err)
+        }
+    }
 }
 
 fn get_url(hostname: &str, resource: &str, schema: &str) -> String {
