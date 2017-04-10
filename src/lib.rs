7bbc466350ffae3d35e89fb955afaccad75d18ec
Adrian Malacoda <adrian.malacoda@monarch-pass.net>
2017-04-10 00:01:22 -0500

    Add some shortcut functions. Eventually these functions should all return Results

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,12 @@ extern crate log;
 
 pub mod client;
 pub mod resource;
+
+pub fn get_from_host(hostname: &str, resource: &str) -> resource::resource::Resource {
+    resource::resource::from_json(&client::client::get_by_https(hostname, resource))
+}
+
+pub fn get(resource: &str) -> resource::resource::Resource {
+    let hostname = client::urlbuilder::get_hostname(resource).unwrap();
+    get_from_host(&hostname, resource)
+}
