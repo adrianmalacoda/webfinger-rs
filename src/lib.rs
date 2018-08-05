2e4c23a998336941d55cca07dfba55966b2bc5d4
Adrian Malacoda <adrian.malacoda@monarch-pass.net>
2018-08-05 03:50:15 -0500

    add function to fall back to http if https lookup fails. This should obviously only be used in development.

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,8 @@ extern crate log;
 extern crate derive_error;
 
 pub mod client;
+use client::client::{get_by_http, get_by_https};
+
 pub mod resource;
 
 #[derive(Debug, Error)]
@@ -29,12 +31,17 @@ pub enum WebFingerError {
 }
 
 pub fn get_from_host(hostname: &str, resource: &str) -> Result<resource::resource::Resource, WebFingerError> {
-    Ok(serde_json::from_str(&client::client::get_by_https(hostname, resource)?)?)
+    Ok(serde_json::from_str(&get_by_https(hostname, resource)?)?)
+}
+
+pub fn get_from_host_insecure(hostname: &str, resource: &str) -> Result<resource::resource::Resource, WebFingerError> {
+    let response = get_by_https(hostname, resource).or_else(|_| get_by_http(hostname, resource))?;
+    Ok(serde_json::from_str(&response)?)
 }
 
 pub fn get(resource: &str) -> Result<resource::resource::Resource, WebFingerError> {
     match client::urlbuilder::get_hostname(resource)? {
-        Some(hostname) => get_from_host(&hostname, resource),
+        Some(hostname) => get_from_host_insecure(&hostname, resource),
         None => Err(WebFingerError::NoHost)
     }
 }
